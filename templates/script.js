184acd853780eeb6169233c4bcdb1bf6640694b1
BRANDDY <BRANDY.M.824@GMAIL.COM>
2022-03-31 14:45:00 -0400

    test

--- templates/script.js
+++ templates/script.js
@@ -5,18 +5,19 @@ function post(){
 
     var newdiv = document.createElement("div");
     newli.appendChild(newdiv);
-// imag
+
     var hi = document.createElement("h1");
     newli.appendChild(hi);
 
-    var biao = document.getElementById("title");
-    hi.innerText = biao.value;
+    var name = document.getElementById("title");
+    hi.innerText = name.value;
 
     var newp = document.createElement("p");
     newli.appendChild(newp);
 
     var ban = document.getElementById("sec");
-    newp.innerText = "Store:" + ban.value;
+    var cont = document.getElementById("content");
+    newp.innerText = ban.value + ": " + cont.value;
 
     var span = document.createElement("span");
     newp.appendChild(span);
